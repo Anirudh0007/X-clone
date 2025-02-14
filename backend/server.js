72b9b94047d3c8b75b4cdd9742b4d64d2e9c0fa3
Anirudh <anirudhswaminathan20@gmail.com>
2025-02-15 04:50:38 +0530

    3.Auth handled and routes protected in the client

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,7 @@ app.use('/api/posts', postRoutes)
 app.use('/api/notifications',notificationRoutes);
 
 
-app.listen(8000,()=>{
+app.listen(5000,()=>{
     
     console.log(`Server is running on port ${PORT}`);
     connectMongoDB();
